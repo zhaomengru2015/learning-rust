a71b59570a63297a8acdc2f7339409a13cfe8063
mrzhao <zhaomengru2018@gmail.com>
2023-09-16 13:56:01 +0800

    feat: using thread pool

--- src/currency_2.rs
+++ src/currency_2.rs
@@ -1,7 +1,6 @@
-use crossbeam_channel::unbounded;
-use std::collections::HashMap;
-use std::sync::{Arc, Condvar, Mutex};
 use std::thread;
+
+use crossbeam_channel::unbounded;
 
 enum WorkMsg {
     Work(u8),
@@ -17,31 +16,74 @@ enum ResultMsg {
 pub fn servo_channel_1() {
     let (worker_sender, worker_receiver) = unbounded();
     let (result_sender, result_receiver) = unbounded();
+    let (pool_result_sender, pool_result_receiver) = unbounded();
+    let pool = rayon::ThreadPoolBuilder::new()
+        .num_threads(2)
+        .build()
+        .unwrap();
+    let mut ongoing_work = 0;
+    let mut existing = false;
     let handler = thread::spawn(move || loop {
-        match worker_receiver.recv() {
-            Ok(WorkMsg::Work(num)) => {
-                let _ = result_sender.send(ResultMsg::Result(num + 100u8));
-            }
-            Ok(WorkMsg::Exit) => {
-                let _ = result_sender.send(ResultMsg::Exited);
-                break;
-            }
-            _ => panic!("Error receiving a WorkMsg"),
+        // 使用crossbeam选择一个就绪的工作
+        select! {
+            recv(worker_receiver) -> msg =>{
+                match msg {
+                    Ok(WorkMsg::Work(num)) => {
+                        let result_sender = result_sender.clone();
+                        let pool_result_sender = pool_result_sender.clone();
+                        // 池上启动一个新的工作单元
+                        ongoing_work +=1;
+                        pool.spawn( move ||{
+                            //1. 发送结果给主组件
+                            let _ = result_sender.send(ResultMsg::Result(num + 100u8));
+                            //2. 让并行组件知道这里完成了一个工作单元
+                            println!("worker finished work {:?}", ongoing_work);
+                            let _ = pool_result_sender.send(());
+                        });
+                    }
+                    Ok(WorkMsg::Exit) => {
+                        existing  = true;
+                        // 如果没有正在进行的工作，则退出
+                        if ongoing_work == 0 {
+                            let _ = result_sender.send(ResultMsg::Exited);
+                            break;
+                        }
+                    }
+                    _ => panic!("Error receiving a WorkMsg"),
+                }
+            },
+            recv(pool_result_receiver) -> _ =>{
+                println!("pool_result_receiver received finished work {:?}", ongoing_work);
+                if ongoing_work == 0 {
+                    panic!("Received an unexpected pool result");
+                }
+                //一个工作单元已经完成
+                ongoing_work-=1;
+                if ongoing_work == 0 && existing{
+                    let _ = result_sender.send(ResultMsg::Exited);
+                    break;
+                }
+            },
         }
     });
 
     let _ = worker_sender.send(WorkMsg::Work(1));
     let _ = worker_sender.send(WorkMsg::Work(2));
     let _ = worker_sender.send(WorkMsg::Exit);
+    let mut counter = 0;
     loop {
         match result_receiver.recv() {
-            Ok(ResultMsg::Result(num)) => println!("received result {:?}", num),
+            Ok(ResultMsg::Result(num)) => {
+                println!("received result {:?}", num);
+                counter += 1;
+            }
             Ok(ResultMsg::Exited) => {
                 println!("worker finished");
+                assert_eq!(2, counter);
                 break;
             }
             _ => panic!("Error receiving a ResultMsg"),
         }
     }
-    handler.join().unwrap();
+    // handler.join().unwrap();
 }
